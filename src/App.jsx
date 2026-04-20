ceccfb6069240c09d7acb237a3251b1e1c1326e6
Josiah Danielle Gallenero <anyajb014@gmail.com>
2026-04-20 16:25:53 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
