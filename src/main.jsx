beb03cce2eec9699070eae96aa6c87c1efec0015
Josiah Danielle Gallenero <anyajb014@gmail.com>
2026-04-20 16:14:10 +0800

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const Analytics = import.meta.env.PROD 
-  ? (await import('@vercel/analytics/react')).Analytics 
-  : () => null;
+let Analytics = () => null;
+
+if (import.meta.env.PROD) {
+  const { Analytics: VercelAnalytics } = await import('@vercel/analytics/react');
+  Analytics = VercelAnalytics;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {import.meta.env.PROD && <Analytics />}
+    <Analytics />
   </React.StrictMode>,
 )
